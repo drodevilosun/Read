a83842d01a51eac30cd429ec7d530005401804fe
drodevilosun <drodevilosun@gmail.com>
2018-04-13 17:14:43 +0700

    Create read.c

---
+++ read.c
@@ -0,0 +1,10 @@
+tại sao phải có cookie và session
+khi nào thì ko thể khử đệ qui
+mkv với divx khác nhau sao
+h264 với h265 khác nhau sao?
+team hw ng ta design cái hardware decode h264 như thế nào ko
+một program thì có bao nhiêu vùng stack, bao nhiêu vùng heap?
+muốn giàm rủi ro memory leak thì làm thế nào
+phân biệt heap với stack, tại sao phải khử đệ quy, session với cookie khác nhau sao, làm sao lấy được giá trị từ bit 7 tới bit 10 của 1 số kiểu long? memory leak là gì
+làm sao giải quyết resource conflict?
+process với thread khác nhau sao?
